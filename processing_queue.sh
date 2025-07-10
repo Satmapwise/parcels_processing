3f25ea7dcafb9958b84ad6e67bb9e72d8190ad5b
Satmapwise <smay@mapwise.com>
2025-07-10 18:15:58 -0400

    2to3 upgrade

---
+++ processing_queue.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+
+/usr/bin/python3 /srv/tools/python/lib/parcels_convert.py broward current do_all_mapwise 20250708 gisdev
+/usr/bin/python3 /srv/tools/python/lib/parcels_convert.py seminole current do_all_mapwise 20250705 gisdev
+/usr/bin/python3 /srv/tools/python/lib/parcels_convert.py osceola current do_all_mapwise 20250706 gisdev
+/usr/bin/python3 /srv/tools/python/lib/parcels_convert.py lake current do_all_mapwise 20250706 gisdev
+/usr/bin/python3 /srv/tools/python/lib/parcels_convert.py marion current do_all_mapwise 20250705 gisdev
+/usr/bin/python3 /srv/tools/python/lib/parcels_convert.py hernando current do_all_mapwise 20250705 gisdev
+/usr/bin/python3 /srv/tools/python/lib/parcels_convert.py martin current do_all_mapwise 20250630 gisdev
